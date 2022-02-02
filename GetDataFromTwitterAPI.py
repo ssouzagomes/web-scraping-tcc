4b6f27eb5fd7e4847da83453f1284ca62b9b7d44
Samuel Souza <samuel.souzagomes001@gmail.com>
2022-02-01 22:38:00 -0300

    fix get timeline tweets by user id

--- GetDataFromTwitterAPI.py
+++ GetDataFromTwitterAPI.py
@@ -12,29 +12,27 @@ headers = CaseInsensitiveDict()
 headers["Accept"] = "application/json"
 headers["Authorization"] = "Bearer " + BEARER_TOKEN
 
-async def get_followers(id):
-  url = "https://api.twitter.com/2/users/"+id+"/followers"
+async def tweets_timeline(user_ids):
+  for user_id in user_ids:
+    url = "https://api.twitter.com/2/users/"+user_id+"/tweets"
 
-  response = requests.get(url, headers=headers)
+    response = requests.get(url, headers=headers)
 
-  followers = json.loads(response.text)['data']
+    if('data' in json.loads(response.text)):
+      tweets = json.loads(response.text)['data']
 
-  print(followers)
-
-
-async def liked_tweets(id):
-  url = "https://api.twitter.com/2/users/"+id+"/liked_tweets"
-
-  response = requests.get(url, headers=headers)
-
-  liked = json.loads(response.text)['data']
-
-  print(liked)
+      with open(user_id+'tweets.tsv', 'wt') as out_file:
+        for tweet in tweets:
+          for column in tweet:
+            tsv_writer = csv.writer(out_file, delimiter='\t')
+            tsv_writer.writerow([column, tweet[column]])
 
 
 async def main():
   try:
-    url = "https://api.twitter.com/2/users/by?usernames=EpicGames&user.fields=description,location,url,created_at,public_metrics"
+    usernames = 'EpicGames,samuel_ufop'
+
+    url = "https://api.twitter.com/2/users/by?usernames="+usernames+"&user.fields=description,location,url,created_at,public_metrics,protected"
 
     response = requests.get(url, headers=headers)
 
@@ -46,10 +44,13 @@ async def main():
           tsv_writer = csv.writer(out_file, delimiter='\t')
           tsv_writer.writerow([column, user[column]])
 
-    userId = users[0]['id']
+    user_ids = []
 
-    # await get_followers(userId)
-    # await liked_tweets(id)
+    for user in users:
+      if(user['protected'] == False):
+        user_ids.append(user['id'])
+
+    await tweets_timeline(user_ids)
     
   except HTTPError as http_error:
     print('HTTP error occurred: ' + http_error)
