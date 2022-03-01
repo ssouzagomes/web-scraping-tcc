f62246be713bd350d3f42b4663d296fd0eb58f6a
Samuel Souza <samuel.souzagomes001@gmail.com>
2022-03-01 17:22:16 -0300

    create schema

--- src/main.py
+++ src/main.py
@@ -12,18 +12,23 @@ from modules.epic_games import GetGamesData
 print('### CHOOSE ONE OPTION ###\n')
 
 option = input("1 - Create schema\n" +
-               "2 - Get twitter users data\n" +
-               "3 - Get games data\n\n")
+               "2 - Get games data\n" +
+               "3 - Get previews games\n" +
+               "4 - Get twitter users data\n\n")
 
 if option == "1":
   cursor, connection = asyncio.run(DatabaseConnection.execute())
   asyncio.run(CreateSchema.execute(cursor, connection))
 
 elif option == "2":
-  asyncio.run(GetUsersData.execute(TwitterAuthenticate.headers))
+  asyncio.run(GetGamesData.execute())
 
 elif option == "3":
-  asyncio.run(GetGamesData.execute())
+  print('previews')
+  # asyncio.run(GetGamesData.execute())
+
+elif option == "4":
+  asyncio.run(GetUsersData.execute(TwitterAuthenticate.headers))
 
 else:
   print('Opção inválida!')
--- src/shared/migrations/CreateSchema.py
+++ src/shared/migrations/CreateSchema.py
@@ -1,7 +1,7 @@
 async def execute(cursor, connection):
   cursor.execute(
-    """CREATE TABLE IF NOT EXISTS game (
-      id uuid NOT NULL DEFAULT uuid_generate_v4(),
+    """CREATE TABLE IF NOT EXISTS games (
+      id INT UNIQUE NOT NULL,
       name character varying NOT NULL,
       price character varying NOT NULL,
       release_date timestamp NOT NULL,
@@ -15,21 +15,77 @@ async def execute(cursor, connection):
   )
 
   cursor.execute(
-    """CREATE TABLE IF NOT EXISTS necessary_hardware (
+    """CREATE TABLE IF NOT EXISTS specifications (
       id uuid NOT NULL DEFAULT uuid_generate_v4(),
       operacional_system character varying NOT NULL,
       processor character varying NOT NULL,
       memory character varying,
       graphics character varying,
-      PRIMARY KEY (id)
+      storage character varying,
+      languages character varying,
+      game_id INT NOT NULL,
+      PRIMARY KEY (id),
+      FOREIGN KEY (game_id) REFERENCES games (id)
     );"""
   )
 
   cursor.execute(
-    """ALTER TABLE necessary_hardware
-      ADD CONSTRAINT fk_game_id FOREIGN KEY (game_id)
-      REFERENCES game(id) ON DELETE CASCADE ON UPDATE CASCADE
-    """
+    """CREATE TABLE IF NOT EXISTS critic (
+      id uuid NOT NULL DEFAULT uuid_generate_v4(),
+      company character varying,
+      author character varying NOT NULL,
+      rating character varying NOT NULL,
+      comment character varying NOT NULL,
+      date timestamp NOT NULL,
+      top_critic BOOLEAN NOT NULL DEFAULT FALSE,
+      game_id INT NOT NULL,
+      PRIMARY KEY (id),
+      FOREIGN KEY (game_id) REFERENCES games (id)
+    );"""
+  )
+
+  cursor.execute(
+    """CREATE TABLE IF NOT EXISTS twitter_accounts (
+      id uuid NOT NULL DEFAULT uuid_generate_v4(),
+      name character varying NOT NULL,
+      username character varying NOT NULL,
+      bio character varying,
+      location character varying,
+      website character varying,
+      join_date timestamp NOT NULL,
+      following INT NOT NULL,
+      followers INT NOT NULL,
+      game_id INT NOT NULL,
+      PRIMARY KEY (id),
+      FOREIGN KEY (game_id) REFERENCES games (id)
+    );"""
+  )
+
+  cursor.execute(
+    """CREATE TABLE IF NOT EXISTS tweets (
+      id uuid NOT NULL DEFAULT uuid_generate_v4(),
+      text character varying NOT NULL,
+      url_media character varying,
+      quantity_likes INT,
+      quantity_retweets INT,
+      quantity_quotes INT,
+      quantity_comments INT,
+      timestamp timestamp NOT NULL,
+      twitter_account_id uuid NOT NULL,
+      PRIMARY KEY (id),
+      FOREIGN KEY (twitter_account_id) REFERENCES twitter_accounts (id)
+    );"""
+  )
+
+  cursor.execute(
+    """CREATE TABLE IF NOT EXISTS social_networks (
+      id uuid NOT NULL DEFAULT uuid_generate_v4(),
+      description character varying NOT NULL,
+      url character varying NOT NULL,
+      game_id INT NOT NULL,
+      PRIMARY KEY (id),
+      FOREIGN KEY (game_id) REFERENCES games (id)
+    );"""
   )
 
   cursor.close()
